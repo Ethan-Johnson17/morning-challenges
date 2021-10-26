f6fc32266066bb33588eb9633534f476589d8cf2
Ethan Johnson <ethan.t.johnson1417@gmail.com>
2021-10-26 09:39:13 -0600

    Oct 26

--- app.js
+++ app.js
@@ -12,7 +12,78 @@ function findLongestWord(string) {
     return word
 }
 
-console.log(findLongestWord("This is the longest word in the entire sentence , maybe even in the whole world."), longest)
+console.log(findLongestWord("This is the longest word in the entire aibohphobia , maybe even in the whole world."), longest)
 
 // Establish the facts first, then write the function
 
+function isPalindrome(str) {
+    // Method #1
+    // let arrayValues = string.split('')
+    // let reverseArray = arrayValues.reverse()
+    // let reverseString = reverseArray.join('')
+
+    // if (str == reverseString) {
+    //     console.log('is a palindrome')
+    // }
+    // else { console.log('it is not a palindrome') }
+
+    // Method #2
+    // return str == str.split('').reverse().join('')
+
+    // Method #3
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+        let charStart = str[i]
+        let charEnd = str[str.length - 1 - i]
+        if (charStart !== charEnd) {
+            return false
+        }
+    }
+    return true
+}
+
+
+isPalindrome('racecar')
+
+//common letter
+function getMax(str) {
+    let max = 0
+    let maxChar = ''
+    str.split('').forEach(function (char) {
+        if (str.split(char).length > max) {
+            max = str.split(char).length
+            maxChar = char
+        }
+    });
+    return maxChar
+}
+
+console.log('The most common letter is', getMax('supercalifragilisticexpealadocious'))
+
+//input string
+function mostCommonLetter(str) {
+    let seen = {}
+    let highestCount = 0
+    let highestChar = ''
+    //look at each letter
+    for (let i = 0; i < str.length; i++) {
+        let letter = str[i]
+        //write it down if I haven't seen it before, tally.
+        if (!seen[letter]) {
+            seen[letter] = 1
+        } else {
+            seen[letter]++
+            //if I have seen it before, increase tally
+            //compare count of each character,
+            if (highestCount < seen[letter]) {
+                highestCount = seen[letter]
+                highestChar = letter
+                //update highest based on character counts
+            }
+
+        }
+    }
+    return highestChar
+}
+
+mostCommonLetter('hello')
+//output string (character)
